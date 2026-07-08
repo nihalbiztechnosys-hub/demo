2fe5e9597fb7bc0807294c6e3a79d82ba80b4054
nihalbiztechnosys-hub <nihal.biztechnosys@gmail.com>
2026-07-08 14:21:58 +0530

    report update

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: '.',
   testMatch: '**/*.spec.ts',
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
 });
